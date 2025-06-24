2a181ee2f385fe645b6f54572a3c9053af09f9dc
GitEngHar <harukii1999@gmail.com>
2025-06-24 09:26:03 +0900

    fix

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest(ctx context.Context, name string) (string, error) {
-	return "Hello, " + name + "!", nil
+func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// クエリパラメータから name を取り出し
+	name := req.QueryStringParameters["name"]
+	if name == "" {
+		name = "World"
+	}
+
+	// レスポンス本文
+	body := fmt.Sprintf("Hello, %s!", name)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "text/plain"},
+		Body:       body,
+	}, nil
 }
 
 func main() {
